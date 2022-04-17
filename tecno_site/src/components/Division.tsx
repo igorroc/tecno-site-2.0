945f74ecc68204212e7af9cc1e26d4e53f98fa4f
Igor Rocha <igor_roc@hotmail.com.br>
2022-04-17 15:44:59 -0300

    [fix]: Correctly adding spacement

--- tecno_site/src/components/Division.tsx
+++ tecno_site/src/components/Division.tsx
@@ -1,22 +1,45 @@
 import { styled } from "@stitches/react"
 
 const Div = styled("div", {
+	width: "100%",
+	$$size: "0px",
+
 	variants: {
 		height: {
 			large: {
-				height: "500px",
+				$$size: "500px",
+			},
+			normal: {
+				$$size: "100px",
 			},
 			small: {
-				height: "200px",
+				$$size: "50px",
+			},
+		},
+		line: {
+			true: {
+				margin: "calc($$size/2) 0",
+				backgroundColor: "#fff2",
+				height: "2px",
+			},
+			false: {
+				margin: "calc($$size/2) 0",
+				height: "$$size",
 			},
 		},
 	},
 })
 
 type DivisionProps = {
-	height: "large" | "small",
+	height: "large" | "normal" | "small"
+	line?: boolean
 	children?: React.ReactNode
 }
+
 export function Division(props: DivisionProps) {
-	return <Div height={props.height}>{props.children}</Div>
+	return (
+		<Div height={props.height} line={props.line}>
+			{props.children}
+		</Div>
+	)
 }
