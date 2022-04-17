0a44be9142e963b431ca532fad75e4f1ec5283dd
Igor Rocha <igor_roc@hotmail.com.br>
2022-04-17 12:16:38 -0300

    [del]: Unused component

--- tecno_site/src/pages/Contact.tsx
+++ tecno_site/src/pages/Contact.tsx
@@ -6,26 +6,7 @@ import { MainText } from "../components/MainText"
 import { Button } from "../components/Button"
 import { MaxWidthWrapper } from "../components/MaxWidthWrapper"
 import { BackgroundImage } from "../components/BackgroundImage"
-
-const WrapperContent = styled("div", {
-	position: "relative",
-	width: "100%",
-	display: "flex",
-	flexDirection: "column",
-	gap: "45px",
-
-	marginTop: "6em",
-
-	"& > h1": {
-		fontSize: "3em",
-		width: "max-content",
-	},
-
-	"& > p": {
-		fontSize: "1.2em",
-		maxWidth: "500px",
-	},
-})
+import { WrapperMainContent } from "../components/WrapperMainContent"
 
 function Contact() {
 	return (
@@ -33,11 +14,11 @@ function Contact() {
 			<Header active="Contato"></Header>
 			<BackgroundImage image={mainBg} fullHeight></BackgroundImage>
 			<MaxWidthWrapper>
-				<WrapperContent>
+				<WrapperMainContent>
 					<h1>
 						Entre em<MainText>contato</MainText>
 					</h1>
-				</WrapperContent>
+				</WrapperMainContent>
 			</MaxWidthWrapper>
 		</div>
 	)
