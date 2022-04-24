db0c7d2edfb1cd045f237cf36cbb4c3e0b928dff
Igor Rocha <igor_roc@hotmail.com.br>
2022-04-24 14:57:12 -0300

    [add]: Slider carrousel

--- tecno_site/src/components/Carrousel.tsx
+++ tecno_site/src/components/Carrousel.tsx
@@ -1,42 +1,104 @@
 import { styled } from "@stitches/react"
+import { Icon } from "./Icon"
+
+import { IconPrefix, IconName } from "@fortawesome/fontawesome-svg-core"
+
+const CarrouselWrapper = styled("div", {
+	position: "relative",
+	width: "100%",
+	minWidth: "1000px",
+	display: "flex",
+	gap: "50px",
+	alignItems: "center",
+	justifyContent: "center",
+})
 
 const Card = styled("div", {
-	width: "310px",
-	height: "384px",
-	maxHeight: "1em",
+	position: "relative",
+	height: "max-content",
+	padding: "1em",
+	maxHeight: "11em",
 	fontSize: "1em",
 	overflow: "hidden",
-	background: "linear-gradient(#3A3A3A, #272727, 40%, #131313)",
+	background: "linear-gradient(#3A3A3A, #272727, 20%, #131313)",
+	borderRadius: "5px",
+	display: "flex",
+	flexDirection: "column",
+	gap: "2em",
+	margin: "0 auto",
+
+	transition: "all 500ms ease-in",
 
 	variants: {
 		active: {
 			true: {
-				background: "linear-gradient(#5552DE, #2F34A0, 40%, #132A7D)",
-				maxHeight: "384px",
+				
 			},
+			false: {
+				
+			},
+		},
+	},
+
+	"& h3": {
+		position: "relative",
+		fontSize: "2em",
+
+		"&:after": {
+			content: "",
+			position: "absolute",
+			bottom: "-7px",
+			left: "0",
+			width: "75px",
+			height: "3px",
+			backgroundColor: "#fff",
 		},
 	},
 })
 
+const NodeIcon = styled("div", {
+	width: "3em",
+	height: "3em",
+	padding: "1em",
+	borderRadius: "50%",
+	backgroundColor: "#fff5",
+	display: "flex",
+	justifyContent: "center",
+	alignItems: "center",
+
+	"& svg": {
+		fontSize: "3em",
+	},
+})
+
 type NodeProps = {
-	active?: boolean
+	active?: boolean | false
 	children?: React.ReactNode
 }
 
+type IconProps = {
+	prefix: IconPrefix
+	iconName: IconName
+}
+
 export function Carrousel(props: NodeProps) {
-	return <div>{props.children}</div>
+	return <CarrouselWrapper>{props.children}</CarrouselWrapper>
 }
 
 export function CarrouselNode(props: NodeProps) {
-	return <Card active={props.active}>{props.children}</Card>
+	return <Card active={props.active || false}>{props.children}</Card>
 }
 
-export function CarrouselNodeIcon() {
-	return <i className="fa-solid fa-arrow-right"></i>
+export function CarrouselNodeIcon(props: IconProps) {
+	return (
+		<NodeIcon>
+			<Icon iconName={props.iconName} prefix={props.prefix} />
+		</NodeIcon>
+	)
 }
 
 export function CarrouselNodeTitle(props: NodeProps) {
-	return <div>{props.children}</div>
+	return <h3>{props.children}</h3>
 }
 
 export function CarrouselNodeContent(props: NodeProps) {
--- tecno_site/src/pages/Home.tsx
+++ tecno_site/src/pages/Home.tsx
@@ -18,13 +18,55 @@ import {
 import { SectionTitle } from "../components/SectionTitle"
 import { Icon } from "../components/Icon"
 import { Footer } from "../components/Footer"
-import GlobalDiv from "../components/GlobalDiv"
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+import Slider from "react-slick"
+import { useState } from "react"
+
+type Provider = { active: boolean }
 
 function Home() {
+	const [services, setServices] = useState<Provider[]>([
+		{ active: true },
+		{ active: false },
+		{ active: false },
+		{ active: false },
+		{ active: false },
+		{ active: false },
+	])
+
+	const carrouselOnChange = (velho: Number, novo: Number) => {
+		let newArray = []
+		newArray.push(...services)
+		newArray.map((i, index) => {
+			if (index === velho) {
+				i.active = false
+			}
+			if (index === novo) {
+				i.active = true
+			}
+		})
+
+		setServices([...newArray])
+	}
+
+	const settings = {
+		className: "centerCarrousel",
+		centerMode: true,
+		infinite: true,
+		centerPadding: "60px",
+		slidesToShow: 3,
+		speed: 300,
+		focusOnSelect: true,
+		// beforeChange: carrouselOnChange,
+	}
+
 	return (
 		<GlobalDiv>
 			<Header active="Home"></Header>
+
 			<BackgroundImage image={mainBg}></BackgroundImage>
+
 			<MaxWidthWrapper>
 				{/* Main Section */}
 				<WrapperMainContent>
@@ -49,9 +91,20 @@ function Home() {
 				<SectionTitle title="Serviços" color="blue">
 					Conheça os nossos <strong>serviços</strong>!
 				</SectionTitle>
-				<Carrousel>
-					<CarrouselNode>
-						<CarrouselNodeIcon />
+				<Slider {...settings}>
+					<CarrouselNode active={services[0].active}>
+						<CarrouselNodeIcon iconName="globe" prefix="fas" />
+						<CarrouselNodeTitle>Websites</CarrouselNodeTitle>
+						<CarrouselNodeContent>
+							Desenvolvemos desde uma página simples de portfólio,
+							até um site institucional com várias páginas.
+						</CarrouselNodeContent>
+					</CarrouselNode>
+					<CarrouselNode active={services[1].active}>
+						<CarrouselNodeIcon
+							iconName="mobile-button"
+							prefix="fas"
+						/>
 						<CarrouselNodeTitle>Aplicativos</CarrouselNodeTitle>
 						<CarrouselNodeContent>
 							Nós desenvolvemos aplicativos nativos de alta
@@ -59,16 +112,8 @@ function Home() {
 							clientes e seu bolso.
 						</CarrouselNodeContent>
 					</CarrouselNode>
-					<CarrouselNode active>
-						<CarrouselNodeIcon />
-						<CarrouselNodeTitle>Websites</CarrouselNodeTitle>
-						<CarrouselNodeContent>
-							Desenvolvemos desde uma página simples de portfólio,
-							até um site institucional com várias páginas.
-						</CarrouselNodeContent>
-					</CarrouselNode>
-					<CarrouselNode>
-						<Icon prefix="fas" iconName="arrow-right" />
+					<CarrouselNode active={services[2].active}>
+						<CarrouselNodeIcon iconName="server" prefix="fas" />
 						<CarrouselNodeTitle>Sistemas Web</CarrouselNodeTitle>
 						<CarrouselNodeContent>
 							Usamos das melhores tecnologias do mercado para
@@ -77,12 +122,35 @@ function Home() {
 							e-commerce, saia com a qualidade esperada.
 						</CarrouselNodeContent>
 					</CarrouselNode>
-				</Carrousel>
+					<CarrouselNode active={services[3].active}>
+						<CarrouselNodeIcon
+							iconName="mobile-button"
+							prefix="fas"
+						/>
+						<CarrouselNodeTitle>Aplicativos</CarrouselNodeTitle>
+						<CarrouselNodeContent>
+							Nós desenvolvemos aplicativos nativos de alta
+							performance para atender sua necessidade, seus
+							clientes e seu bolso.
+						</CarrouselNodeContent>
+					</CarrouselNode>
+					<CarrouselNode active={services[4].active}>
+						<CarrouselNodeIcon iconName="server" prefix="fas" />
+						<CarrouselNodeTitle>Sistemas Web</CarrouselNodeTitle>
+						<CarrouselNodeContent>
+							Usamos das melhores tecnologias do mercado para
+							desenvolver sua aplicação. Garantindo, que aquele
+							sistema que gerencia sua barbearia ou que seu
+							e-commerce, saia com a qualidade esperada.
+						</CarrouselNodeContent>
+					</CarrouselNode>
+				</Slider>
 				<SectionTitle title="Orçamento" color="purple"></SectionTitle>
 				<SectionTitle title="Parceiros" color="blue">
 					Conheça quem confia na TecnoJr!
 				</SectionTitle>
 			</MaxWidthWrapper>
+
 			<Footer />
 		</GlobalDiv>
 	)
