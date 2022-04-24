9b6482f1aca385ef21e24e69519ac2f6634ff8f2
Igor Rocha <igor_roc@hotmail.com.br>
2022-04-24 17:22:30 -0300

    [add]: Members photos and infos

--- tecno_site/src/common/membersInfo.tsx
+++ tecno_site/src/common/membersInfo.tsx
@@ -1,12 +1,72 @@
 import imgIgorRocha from "../assets/img/members/igor_rocha.png"
+import imgEvelyn from "../assets/img/members/evelyn.png"
+import imgGabiZerbone from "../assets/img/members/gabi_zerbone.png"
+import imgIsabelle from "../assets/img/members/isabelle.png"
+import imgThalles from "../assets/img/members/thalles_cerqueira.png"
 import memberImg from "../assets/img/members/photo.png"
 
 const memberList = [
+	{
+		name: "Isabelle Cruz",
+		role: "Diretora de Presidente",
+		img: imgIsabelle,
+		links: [
+			{
+				icon: "instagram",
+				prefix: "fab",
+				url: "https://instagram.com/iamisabellec",
+			},
+			{
+				icon: "linkedin",
+				prefix: "fab",
+				url: "https://www.linkedin.com/in/isabellesscruz",
+			},
+		],
+	},
 	{
 		name: "Igor Rocha",
 		role: "Diretor de Projetos",
 		img: imgIgorRocha,
 		links: [
+			{
+				icon: "globe",
+				prefix: "fas",
+				url: "https://igorroc.github.io/igorroc/",
+			},
+			{
+				icon: "github",
+				prefix: "fab",
+				url: "https://github.com/igorroc/",
+			},
+			{
+				icon: "envelope",
+				prefix: "fas",
+				url: "mailto:ilrocha.cic@uesc.br",
+			},
+			{
+				icon: "linkedin",
+				prefix: "fab",
+				url: "https://www.linkedin.com/in/igorroc",
+			},
+		],
+	},
+	{
+		name: "Thalles Cerqueira",
+		role: "Diretor de Vendas",
+		img: imgThalles,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Gabriella Zerbone",
+		role: "Diretora de RH",
+		img: imgGabiZerbone,
+		links: [
 			{ icon: "github", prefix: "fab", url: "https://www.github.com" },
 			{
 				icon: "linkedin",
@@ -16,8 +76,8 @@ const memberList = [
 		],
 	},
 	{
-		name: "Isabelle Cruz",
-		role: "Diretora Presidente",
+		name: "Mateus Lisboa",
+		role: "Diretor Financeiro",
 		img: memberImg,
 		links: [
 			{
@@ -28,8 +88,20 @@ const memberList = [
 		],
 	},
 	{
-		name: "Thalles Cerqueira",
-		role: "Diretor de Vendas",
+		name: "Evelyn Santos",
+		role: "Conselheira",
+		img: imgEvelyn,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Marcus Vinicius",
+		role: "Conselheiro",
 		img: memberImg,
 		links: [
 			{
@@ -40,21 +112,8 @@ const memberList = [
 		],
 	},
 	{
-		name: "Igor Lima Rocha Rocha",
-		role: "Diretor de Projetos",
-		img: memberImg,
-		links: [
-			{ icon: "github", prefix: "fab", url: "https://www.github.com" },
-			{
-				icon: "linkedin",
-				prefix: "fab",
-				url: "https://www.linkedin.com",
-			},
-		],
-	},
-	{
-		name: "Isabelle Cruz",
-		role: "Diretora Presidente",
+		name: "Brendon Wilyen",
+		role: "Membro",
 		img: memberImg,
 		links: [
 			{
@@ -65,8 +124,128 @@ const memberList = [
 		],
 	},
 	{
-		name: "Thalles Cerqueira",
-		role: "Diretor de Vendas",
+		name: "Jefferson Ferreira",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Juliana Midlej",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Eric Soares",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Daniel Jackson",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Luca Sacramento",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Alanis Rodrigues",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Breno Vitório",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Daniel Oliveira",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Igor Nascimento",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Yan Costa",
+		role: "Membro",
+		img: memberImg,
+		links: [
+			{
+				icon: "facebook",
+				prefix: "fab",
+				url: "https://www.facebook.com",
+			},
+		],
+	},
+	{
+		name: "Victor José",
+		role: "Membro",
 		img: memberImg,
 		links: [
 			{
