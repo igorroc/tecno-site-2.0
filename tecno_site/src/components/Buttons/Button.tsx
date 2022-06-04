58160efde80474beb181af6aff9c328cd8402769
Igor Rocha <igor_roc@hotmail.com.br>
2022-06-03 22:19:49 -0300

    [add]: New tab prop

--- tecno_site/src/components/Buttons/Button.tsx
+++ tecno_site/src/components/Buttons/Button.tsx
@@ -35,12 +35,17 @@ const CustomButton = styled("button", {
 type ButtonProps = {
 	url?: string
 	children?: React.ReactNode
+	newTab?: boolean
 }
 
 export function Button(props: ButtonProps) {
 	function linkToPage() {
 		if (props.url) {
-			window.location.replace(props.url)
+			if (props.newTab) {
+				window.open(props.url, "_blank")?.focus()
+			} else {
+				window.location.replace(props.url)
+			}
 		}
 	}
 	return <CustomButton onClick={linkToPage}>{props.children}</CustomButton>
