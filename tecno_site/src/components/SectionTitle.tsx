96a9fc78ab1e398bd2ee032397ec2ce971cb6603
Igor Rocha <igor_roc@hotmail.com.br>
2022-06-03 22:19:36 -0300

    [add]: Id handler

--- tecno_site/src/components/SectionTitle.tsx
+++ tecno_site/src/components/SectionTitle.tsx
@@ -49,10 +49,11 @@ type TitleProps = {
 	color: "blue" | "purple"
 	title?: string
 	children?: React.ReactNode
+	id?: string
 }
 export function SectionTitle(props: TitleProps) {
 	return (
-		<Div color={props.color}>
+		<Div color={props.color} id={props.id}>
 			<span id={props.title?.toLowerCase()}>{props.title}</span>
 			<p>{props.children}</p>
 		</Div>
