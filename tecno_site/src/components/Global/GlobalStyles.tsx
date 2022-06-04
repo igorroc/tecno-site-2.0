200144d6debe7a7b378a45d705c59e23fcaa121f
Igor Rocha <igor_roc@hotmail.com.br>
2022-06-03 21:17:43 -0300

    [fix]: Better carousel

--- tecno_site/src/components/Global/GlobalStyles.tsx
+++ tecno_site/src/components/Global/GlobalStyles.tsx
@@ -25,41 +25,23 @@ const globalStyles = globalCss({
 		fontSize: "1em",
 		color: "#31e7ff",
 	},
+
 	".centerCarrousel": {
-		"& .slick-slide > div > div": {
+		"& .slick-slide": {
+			"&.slick-current > div > div": {
+				boxShadow: "0px 10px 20px #000a",
+				transform: "scale(1.1)",
+			},
+			"& > div > div": {
+				boxShadow: "unset",
+
 			"& h3:after": {
 				backgroundColor: "#5A7BE7",
 			},
-
-			"&:after": {
-				content: "",
-				position: "absolute",
-				top: "0",
-				left: "0",
-				width: "100%",
-				height: "100%",
-
-				background: "linear-gradient(#0000 50%, #131313 95%)",
-			},
 		},
 
-		"& .slick-slide > div": {
+			"& > div": {
 			margin: "20px",
-		},
-
-		"& .slick-center > div > div": {
-			background: "linear-gradient(#5552DE, #2F34A0, 40%, #132A7D)",
-			maxHeight: "30em",
-
-			"&:after": {
-				content: "",
-				position: "absolute",
-				top: "0",
-				left: "0",
-				width: "100%",
-				height: "100%",
-
-				background: "none",
 			},
 		},
 	},
