567d395d2a7c9b8a7043eddfcc3f87ff2024e284
Igor Rocha <igor_roc@hotmail.com.br>
2023-05-23 22:32:06 -0300

    fix: Projetos

---
+++ src/common/projectsInfo.ts
@@ -0,0 +1,83 @@
+export const externalProjects = [
+	{
+		title: "NASA Space Terra",
+		description: "Website",
+		link: "https://spaceterra.org/",
+		img: "spaceterra.jpg",
+	},
+	{
+		title: "NASA Space Apps",
+		description: "Website",
+		link: "https://nasa.spaceterra.org/",
+		img: "nasaapps.png",
+	},
+	{
+		title: "Semana de Inovação",
+		description: "Website",
+		link: "https://semanadeinovacaoilheus.com.br/",
+		img: "semana_inovacao.png",
+	},
+	{
+		title: "SIMMBRA",
+		description: "WebSite",
+		link: "https://simmbra.com.br",
+		img: "simmbra.png",
+	},
+	{
+		title: "NASA Space Apps Salvador",
+		description: "Website",
+		link: "https://salvador.spaceterra.org/",
+		img: "appssalvador.png",
+	},
+	{
+		title: "Afusc",
+		description: "WebSite",
+		link: "https://afuscuesc.com",
+		img: "afusc.png",
+	},
+	{
+		title: "GuiaServe",
+		description: "Sistema Web + App",
+		link: "https://guiaserve.com.br",
+		img: "guiaserve.png",
+	},
+]
+
+export const internalProjects = [
+	{
+		title: "COLCIC UESC",
+		author: "Igor Rocha",
+		link: "https://colcic.uesc.br",
+		img: "colcic.png",
+	},
+	{
+		title: "Database UESC",
+		author: "Igor Rocha",
+		link: "https://uesc.ilrocha.com/emails",
+		img: "databaseuesc.png",
+	},
+	{
+		title: "Calculadora de Notas",
+		author: "Igor Rocha",
+		link: "https://igorroc.github.io/finalcountdown/",
+		img: "calculadoranotas.png",
+	},
+	{
+		title: "Wiki de Suporte (LOL)",
+		author: "Murilo Maia",
+		link: "https://murilopmaia.github.io/projeto_final/",
+		img: "wikilol.jpg",
+	},
+	{
+		title: "Estarolas",
+		author: "Joabe Ferreira",
+		link: "https://gannjobs.github.io/ProjetoWebTecno/",
+		img: "estarolas.jpg",
+	},
+	{
+		title: "Pokedex",
+		author: "Solana Ametista",
+		link: "http://solana-ametista.github.io",
+		img: "pokedex.jpg",
+	},
+]
--- src/pages/Projetos/index.tsx
+++ src/pages/Projetos/index.tsx
@@ -14,96 +14,14 @@ import { SectionTitle } from "../../components/SectionTitle"
 import CardsWrapper, { Card } from "../../components/Card"
 
 import { motion } from "framer-motion"
-
-const externalProjects = [
-	{
-		title: "NASA Space Terra",
-		description: "Website",
-		link: "https://spaceterra.org/",
-		img: "spaceterra.jpg",
-	},
-	{
-		title: "NASA Space Apps",
-		description: "Website",
-		link: "https://nasa.spaceterra.org/",
-		img: "nasaapps.png",
-	},
-	{
-		title: "Semana de Inovação",
-		description: "Website",
-		link: "https://semanadeinovacaoilheus.com.br/",
-		img: "semana_inovacao.png",
-	},
-	{
-		title: "SIMMBRA",
-		description: "WebSite",
-		link: "https://simmbra.com.br",
-		img: "simmbra.png",
-	},
-	{
-		title: "NASA Space Apps Salvador",
-		description: "Website",
-		link: "https://salvador.spaceterra.org/",
-		img: "appssalvador.png",
-	},
-	{
-		title: "Afusc",
-		description: "WebSite",
-		link: "https://afuscuesc.com",
-		img: "afusc.png",
-	},
-	{
-		title: "Partiu Dirigir",
-		description: "Sistema Web",
-		link: "https://partiu.tecnojr.com.br",
-		img: "partiu.png",
-	},
-]
-
-const internalProjects = [
-	{
-		title: "COLCIC UESC",
-		author: "Igor Rocha",
-		link: "https://colcic.uesc.br",
-		img: "colcic.png",
-	},
-	{
-		title: "Database UESC",
-		author: "Igor Rocha",
-		link: "https://uesc.ilrocha.com/emails",
-		img: "databaseuesc.png",
-	},
-	{
-		title: "Calculadora de Notas",
-		author: "Igor Rocha",
-		link: "https://igorroc.github.io/finalcountdown/",
-		img: "calculadoranotas.png",
-	},
-	{
-		title: "Wiki de Suporte (LOL)",
-		author: "Murilo Maia",
-		link: "https://murilopmaia.github.io/projeto_final/",
-		img: "wikilol.jpg",
-	},
-	{
-		title: "Estarolas",
-		author: "Joabe Ferreira",
-		link: "https://gannjobs.github.io/ProjetoWebTecno/",
-		img: "estarolas.jpg",
-	},
-	{
-		title: "Pokedex",
-		author: "Solana Ametista",
-		link: "http://solana-ametista.github.io",
-		img: "pokedex.jpg",
-	},
-]
+import { externalProjects, internalProjects } from "../../common/projectsInfo"
 
 const animations = {
 	initial: { opacity: 0, y: -50 },
 	animate: { opacity: 1, y: 0 },
 	exit: { opacity: 0, y: -50 },
 }
+
 export default function Projetos() {
 	return (
 		<motion.div
